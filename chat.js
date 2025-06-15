74e173ffcae8c1356d48caa6be2374ef9723bae2
Datta Panchal <panchaldatta508@gmail.com>
2025-06-15 15:18:42 +0530

    chat function

---
+++ chat.js
@@ -0,0 +1,11 @@
+var name="Datta"
+var age = 24
+var isProgrammer = true
+
+function  summeriseProfile(UserName,UserAge,UserIsProgrammer) {
+   return (
+     `My name is ${UserName}, I am ${UserAge} years old and it is ${UserIsProgrammer} that I am a programmer.`)
+
+};
+ 
+console.log(summeriseProfile(name, age, isProgrammer));
