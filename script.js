d14d9a00789342edb99e567c1796d69c9114498d
Datta Panchal <panchaldatta508@gmail.com>
2025-06-28 22:36:50 +0530

    Keywords in JS

---
+++ script.js
@@ -0,0 +1,22 @@
+// var a = 12;
+
+//var function scoped hai
+var z=46;
+let a=10
+const  offer = 15;
+
+console.log(a, z, offer);
+
+//global scope
+//functional scope
+//block scope
+
+var abc=11
+abc=32;
+
+let xyz = 77;
+xyz=45;
+
+
+
+
